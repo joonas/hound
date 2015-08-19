1141e49a3fc81fde0d12cad163f8e43fd011636e
Elliot Winkler <elliot.winkler@gmail.com>
2015-08-19 12:12:03 -0600

    Add tests

--- spec/models/style_guide/coffee_script_spec.rb
+++ spec/models/style_guide/coffee_script_spec.rb
@@ -87,6 +87,21 @@ describe StyleGuide::CoffeeScript do
       end
     end
 
+    context "with thoughtbot configuration" do
+      context "for an empty function" do
+        it "returns a file review without any violations" do
+          code = <<-CODE.strip_heredoc
+            class FooBar
+              foo: ->
+          CODE
+
+          violations = violations_in(code, repository_owner_name: "thoughtbot")
+
+          expect(violations).not_to be_any { |m| m =~ /Empty function/ }
+        end
+      end
+    end
+
     context "with violation on unchanged line" do
       it "finds no violations" do
         file = double(
